a2f1ae1e318343f6620b088a257ee4b8f3f2ab0c
ASMlover <zfxfcx@163.com>
2012-07-17 17:24:39 +0800

    added testcase of arena mamngement module.

---
+++ test/test_arena.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2012 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../inc/arena.h"
+
+#include "test.h"
+
+
+
+typedef struct _TestArena {
+  int id;
+  char name[32];
+} TestArena;
+
+void test_arena(void)
+{
+  int arena = arena_create();
+
+  fprintf(stdout, "call function : %s\n", __func__);
+  if (0 == arena)
+  {
+    fprintf(stdout, "\tcreate arena object failed\n");
+    goto __quit;
+  }
+  else
+    fprintf(stdout, "\tcreate arena object success\n");
+
+  fprintf(stdout, "\ncall arena_alloc ===>\n");
+  {
+    TestArena* ta = NULL;
+    int i;
+    for (i = 0; i < 10; ++i)
+    {
+      ta = (TestArena*)ARENA_ALLOC(arena, sizeof(TestArena));
+      ta->id = rand() % 1000;
+      sprintf(ta->name, "ARENA_ALLOC-TestArena %d", ta->id);
+
+      fprintf(stdout, "[%02d] TestArena object {id=>%d,name=>'%s'}\n", i, ta->id, ta->name);
+    }
+
+    arena_free(arena);
+  }
+
+  fprintf(stdout, "\ncall arena_calloc ===>\n");
+  {
+    TestArena* ta;
+    int i;
+    for (i = 0; i < 10; ++i)
+    {
+      ta = (TestArena*)ARENA_CALLOC(arena, 1, sizeof(TestArena));
+      ta->id = rand() % 1000;
+      sprintf(ta->name, "ARENA_CALLOC-TestArena %d", ta->id);
+
+      fprintf(stdout, "[%02d] TestArena object {id=>%d,name=>'%s'}\n", i, ta->id, ta->name);
+    }
+    arena_free(arena);
+  }
+
+__quit:
+  arena_release(&arena);
+}
