c0473ecdcfde7cce243f81b1a151b62cb01b1d16
ASMlover <zfxfcx@163.com>
2012-08-14 14:16:40 +0800

    added implementation of bit module.

--- src/bit.c
+++ src/bit.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2012 ASMlover. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list ofconditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materialsprovided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 #include <stdarg.h>
 #include <string.h>
 #include "../inc/config.h"
@@ -17,6 +45,17 @@ struct lBit {
 #define BITS_PER_DWORD      (8 * sizeof(unsigned long))
 #define NUM_OF_DWORDS(len)  ((((len) + BITS_PER_DWORD - 1) & (~(BITS_PER_DWORD - 1))) / BITS_PER_DWORD)
 #define NUM_OF_BYTES(len)   ((((len) + 8 - 1) & (~(8 - 1))) / 8)
+
+#define SET_OPERATIONS(B1, B2, op)\
+  {\
+    int i;\
+    struct lBit* B;\
+    assert((B1)->size == (B2)->size);\
+    B = bit_create((B1)->size);\
+    for (i = NUM_OF_DWORDS((B1)->size); --i >= 0; )\
+      B->dwords[i] = (B1)->dwords[i] op (B2)->dwords[i];\
+    return B;\
+  }\
 
 static unsigned char msbmask[] = {
   0xFF, 0xFE, 0xFC, 0xF8, 
@@ -233,20 +272,48 @@ void bit_for_each(void* P, void (*visit)(int, int, void*), void* arg)
 
 void* bit_union(void* B1, void* B2)
 {
-  return NULL;
+  if (NULL == B1 && NULL == B2)
+    return NULL;
+  else if (NULL == B1)
+    return bit_copy(B2);
+  else if (NULL == B2)
+    return bit_copy(B1);
+  else 
+    SET_OPERATIONS((struct lBit*)B1, (struct lBit*)B2, |)
 }
 
 void* bit_inter(void* B1, void* B2)
 {
-  return NULL;
+  if (NULL == B1 && NULL == B2)
+    return NULL;
+  else if (NULL == B1)
+    return bit_create(((struct lBit*)B2)->size);
+  else if (NULL == B2)
+    return bit_create(((struct lBit*)B1)->size);
+  else 
+    SET_OPERATIONS((struct lBit*)B1, (struct lBit*)B2, &)
 }
 
 void* bit_minus(void* B1, void* B2)
 {
-  return NULL;
+  if (NULL == B1 && NULL == B2)
+    return NULL;
+  else if (NULL == B1)
+    return bit_create(((struct lBit*)B2)->size);
+  else if (NULL == B2)
+    return bit_copy(B1);
+  else 
+    SET_OPERATIONS((struct lBit*)B1, (struct lBit*)B2, & ~)
 }
 
 void* bit_diff(void* B1, void* B2)
 {
-  return NULL;
+  if (NULL == B1 || NULL == B2)
+    return NULL;
+  else if (NULL == B1)
+    return bit_copy(B2);
+  else if (NULL == B2)
+    return bit_copy(B1);
+  else 
+    SET_OPERATIONS((struct lBit*)B1, (struct lBit*)B2, ^)
 }
